ab3082f52c81efd70d1f4cbae582d31e60421c7f
WEITAO JIANG <weitaoj@outlook.com>
2022-09-22 20:40:07 +1000

    change css

--- src/component/FeedbackForm/FeedbackForm.js
+++ src/component/FeedbackForm/FeedbackForm.js
@@ -1,44 +1,44 @@
-import React, { Component } from 'react'
-import HappinessPicker from '../HappniessPicker/HappinessPicker.js'
+import React, { Component } from "react";
+import HappinessPicker from "../HappniessPicker/HappinessPicker.js";
 
 export default class FeedbackForm extends Component {
-  state = { food: 0, service: 0, money: 0 }
+  state = { food: 0, service: 0, money: 0 };
   addFoodScore = (v) => {
-    this.setState({ food: v })
-  }
+    this.setState({ food: v });
+  };
   addServiceScore = (v) => {
-    this.setState({ service: v })
-  }
+    this.setState({ service: v });
+  };
 
   addValueScore = (v) => {
-    this.setState({ money: v })
-  }
+    this.setState({ money: v });
+  };
 
   render() {
     return (
       <div
         style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
         }}
       >
         <h1>Feedback Form</h1>
-        <h2>Let us know how we didi!</h2>
-        <h3 style={{ textAlign: 'left' }}>Food:{this.state.food}</h3>
-        <HappinessPicker category={'food'} addScore={this.addFoodScore} />
+        <h2>Let us know how we did!</h2>
+        <h3 style={{ textAlign: "left" }}>Food:{this.state.food}</h3>
+        <HappinessPicker category={"food"} addScore={this.addFoodScore} />
 
-        <h3 style={{ textAlign: 'left' }}>Service:{this.state.service}</h3>
-        <HappinessPicker category={'service'} addScore={this.addServiceScore} />
+        <h3 style={{ textAlign: "left" }}>Service:{this.state.service}</h3>
+        <HappinessPicker category={"service"} addScore={this.addServiceScore} />
 
-        <h3 style={{ textAlign: 'left' }}>Value:{this.state.money}</h3>
-        <HappinessPicker category={'value'} addScore={this.addValueScore} />
-        <p style={{ fontSize: '2em', fontWeight: 'bold' }}>
+        <h3 style={{ textAlign: "left" }}>Value:{this.state.money}</h3>
+        <HappinessPicker category={"value"} addScore={this.addValueScore} />
+        <p style={{ fontSize: "2em", fontWeight: "bold" }}>
           Total Score:
           {this.state.food + this.state.service + this.state.money}
           /12
         </p>
       </div>
-    )
+    );
   }
 }
